051aedcb07eeabc96f427ae0bfa4993ba0fba67d
devkmro <kim.rodgers.mirikwa@gmail.com>
2023-08-07 09:12:31 +0300

    experiment in main

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-from learning_algorithms import sarsa
+from learning_algorithms import sarsa, q_learning
+
+num_of_episodes = 1000
+gamma = 1
+alpha = 0.1
+epsilon = 0.1
+
+sarsa.main(num_of_episodes, gamma, alpha, epsilon)
+q_learning.main(num_of_episodes, gamma, alpha, epsilon)
